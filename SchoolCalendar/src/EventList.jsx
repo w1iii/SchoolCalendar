a9510028c7eea2493a6e5c5083ba09e0f8f079a4
w1iii <lomugdanglf.19@gmail.com>
2025-10-16 17:50:14 +0800

    Event list auto adjust to a new column if exceeds max height. Changed the layout of the event section.
    - add delete function for the event list

--- SchoolCalendar/src/EventList.jsx
+++ SchoolCalendar/src/EventList.jsx
@@ -4,29 +4,41 @@ export default function EventList({ events }) {
   );
 
   return (
-    <div className="event-list">
-      <h3>Upcoming Events</h3>
-      {dates.length === 0 && <p>No events yet.</p>}
+    <>
+      <div className="event-section">
 
-      {dates.map((date) => {
-        const formattedDate = new Date(date).toLocaleDateString("en-US", {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        });
-
-        return (
-          <div key={date} className="event-day">
-            <h4>{formattedDate}</h4>
-            <ul>
-              {events[date].map((e, i) => (
-                <li key={i}>{e}</li>
-              ))}
-            </ul>
+        <div className="event-list-header">
+            <h3>Upcoming Events</h3>
           </div>
-        );
-      })}
-    </div>
+
+        <div className="event-list">
+          {dates.length === 0 && <p>No events yet.</p>}
+          {dates.map((date) => {
+            const formattedDate = new Date(date).toLocaleDateString("en-US", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            });
+
+            return (
+              <div key={date} className="event-day">
+                <h4>{formattedDate}</h4>
+                <div className="events">
+                <ul>
+                  {events[date].map((e, i) => (
+                    <li key={i}>
+                      <button> - </button>
+                      {e}
+                    </li>
+                  ))}
+                </ul>
+                </div>
+            </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
